927826ea1f8d95a580881d9525db7295b18f0a5a
Adam <rodriada000@gmail.com>
2019-07-13 16:25:12 -0700

    update: add class to ETL USDA food composition db data into database
    - created ETL class to load .txt data into PantryPlannerContext

---
+++ PantryPlanner/Migrations/USDAFoodCompositionDbETL.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using PantryPlanner.Models;
+using PantryPlanner.Services;
+
+namespace PantryPlanner.Migrations
+{
+    /// <summary>
+    /// This class is used to run a ETL process on the USDA Food Composition Database .txt files
+    /// to load data into <see cref="Ingredient"/> and <see cref="Category"/> Tables
+    /// </summary>
+    /// <remarks>
+    /// The database data is provided via ASCII .txt files with a specific format. The USDA provides a PDF
+    /// that explains the format and contents of each .txt file here: https://www.ars.usda.gov/ARSUserFiles/80400525/Data/SR-Legacy/SR-Legacy_Doc.pdf
+    /// </remarks>
+    public class USDAFoodCompositionDbETL
+    {
+        /// <summary>
+        /// Root folder that contains the .txt files
+        /// </summary>
+        public string TxtRootFolderPath { get; set; }
+
+        /// <summary>
+        /// Mapping of Nutrient Database number (NDBID) used by USDA to the IngredientID used in PantryPlanner system
+        /// </summary>
+        private Dictionary<string, long> NDBIDToIngredientId { get; set; }
+
+        /// <summary>
+        /// Mapping of Food Group Code used by USDA to the CategoryID used in PantryPlanner system
+        /// </summary>
+        private Dictionary<string, long> FoodGroupCodeToCategoryId { get; set; }
+
+        /// <summary>
+        /// Name of file with food group data
+        /// </summary>
+        public const string FoodGroupFileName = "FD_GROUP.txt";
+
+        /// <summary>
+        /// Name of file with food description data
+        /// </summary>
+        public const string FoodDescriptionFileName = "FOOD_DES.txt";
+
+        /// <summary>
+        /// Mapping of the food description column name to the array index
+        /// </summary>
+        private Dictionary<string, int> _foodDescColumnNameToIndex;
+
+        /// <summary>
+        /// Mapping of the food description column name to the array index
+        /// </summary>
+        public Dictionary<string, int> FoodDescColumnNameToIndex
+        {
+            get
+            {
+                if (_foodDescColumnNameToIndex != null)
+                {
+                    return _foodDescColumnNameToIndex;
+                }
+
+                _foodDescColumnNameToIndex = new Dictionary<string, int>();
+                _foodDescColumnNameToIndex.TryAdd("NDBID", 0);
+                _foodDescColumnNameToIndex.TryAdd("FoodGrpCode", 1);
+                _foodDescColumnNameToIndex.TryAdd("LongDesc", 2);
+                _foodDescColumnNameToIndex.TryAdd("ShortDesc", 3);
+                _foodDescColumnNameToIndex.TryAdd("ComName", 4);
+                _foodDescColumnNameToIndex.TryAdd("ManufacName", 5);
+                _foodDescColumnNameToIndex.TryAdd("Survey", 6);
+                _foodDescColumnNameToIndex.TryAdd("Ref_desc", 7);
+                _foodDescColumnNameToIndex.TryAdd("Refuse", 8);
+                _foodDescColumnNameToIndex.TryAdd("SciName", 9);
+                _foodDescColumnNameToIndex.TryAdd("N_Factor", 10);
+                _foodDescColumnNameToIndex.TryAdd("Pro_Factor", 11);
+                _foodDescColumnNameToIndex.TryAdd("Fat_Factor", 12);
+                _foodDescColumnNameToIndex.TryAdd("CHO_Factor", 13);
+
+                return _foodDescColumnNameToIndex;
+            }
+        }
+
+
+        public USDAFoodCompositionDbETL(string folderPathToDbSource)
+        {
+            TxtRootFolderPath = folderPathToDbSource;
+
+            if (TxtRootFolderPath.EndsWith("\\") == false)
+            {
+                TxtRootFolderPath = TxtRootFolderPath + "\\";
+            }
+
+            NDBIDToIngredientId = new Dictionary<string, long>();
+            FoodGroupCodeToCategoryId = new Dictionary<string, long>();
+        }
+
+        public void StartEtlProcess(PantryPlannerContext context)
+        {
+            ParseFoodGroupFileAndInsertIntoCategoryTable(context);
+            ParseFoodDescriptionFileAndInsertIntoIngredientTable(context);
+        }
+
+        private void ParseFoodDescriptionFileAndInsertIntoIngredientTable(PantryPlannerContext context)
+        {
+            // validate file exists
+            string fullPath = TxtRootFolderPath + FoodDescriptionFileName;
+
+            if (File.Exists(fullPath) == false)
+            {
+                throw new FileNotFoundException($"Could not find {FoodDescriptionFileName} at the specified path", fullPath);
+            }
+
+
+            foreach (string line in File.ReadAllLines(fullPath))
+            {
+                // parse line by format rules (each field is seperated by ^)
+                List<string> fieldValues = line.Split("^").ToList();
+
+                string nutrientDBID = fieldValues[FoodDescColumnNameToIndex["NDBID"]].Trim('~');
+                string foodGroupCode = fieldValues[FoodDescColumnNameToIndex["FoodGrpCode"]].Trim('~');
+                string longDescription = fieldValues[FoodDescColumnNameToIndex["LongDesc"]].Trim('~');
+                string commonNames = fieldValues[FoodDescColumnNameToIndex["ComName"]].Trim('~');
+
+                FoodGroupCodeToCategoryId.TryGetValue(foodGroupCode, out long foodGroupCategoryId);
+
+                if (foodGroupCategoryId == 0)
+                {
+                    continue; // unknown food category, lets skip
+                }
+
+
+                Ingredient existingIngredient = context.Ingredient
+                                                    .Where(i => i.CategoryId == foodGroupCategoryId && i.Name == longDescription)
+                                                    .FirstOrDefault();
+
+                if (existingIngredient != null)
+                {
+                    // remember id and skip
+                    NDBIDToIngredientId.TryAdd(nutrientDBID, existingIngredient.IngredientId);
+                    continue;
+                }
+
+                Ingredient newIngredient = new Ingredient()
+                {
+                    Name = longDescription,
+                    CategoryId = foodGroupCategoryId,
+                    IsPublic = true,
+                    DateAdded = DateTime.Now,
+                    Description = ""
+                };
+
+                context.Ingredient.Add(newIngredient);
+                NDBIDToIngredientId.TryAdd(nutrientDBID, newIngredient.IngredientId);
+            }
+
+            context.SaveChanges();
+
+        }
+
+        private void ParseFoodGroupFileAndInsertIntoCategoryTable(PantryPlannerContext context)
+        {
+            // validate file exists
+            string fullPath = TxtRootFolderPath + FoodGroupFileName;
+
+            if (File.Exists(fullPath) == false)
+            {
+                throw new FileNotFoundException($"Could not find {FoodGroupFileName} at the specified path", fullPath);
+            }
+
+
+            // ensure CategoryType 'Ingredient' exists
+            CategoryType ingredientCategoryType;
+
+            if (context.CategoryType.Any(c => c.Name == "Ingredient") == false)
+            {
+                ingredientCategoryType = new CategoryType()
+                {
+                    Name = "Ingredient"
+                };
+
+                context.CategoryType.Add(ingredientCategoryType);
+                context.SaveChanges();
+            }
+            else
+            {
+                ingredientCategoryType = context.CategoryType.Where(c => c.Name == "Ingredient").FirstOrDefault();
+            }
+
+
+            foreach (string line in File.ReadAllLines(fullPath))
+            {
+                // parse line by format rules (each field is seperated by ^)
+                List<string> fieldValues = line.Split("^").ToList();
+
+                // get code and food group name (trim ~ based on format rules)
+                string code = fieldValues[0].Trim('~');
+                string foodGroupDesc = fieldValues[1].Trim('~');
+
+                // check if food group name already exists in context
+                Category existingCategory = context.Category.Where(c => c.Name == foodGroupDesc && c.CategoryTypeId == ingredientCategoryType.CategoryTypeId).FirstOrDefault();
+
+                if (existingCategory != null)
+                {
+                    // already exists so map IDs and skip adding to context
+                    FoodGroupCodeToCategoryId.TryAdd(code, existingCategory.CategoryId);
+                    continue;
+                }
+
+                // add food group as an Ingredient category
+                Category newFoodGroup = new Category()
+                {
+                    CategoryTypeId = ingredientCategoryType.CategoryTypeId,
+                    Name = foodGroupDesc
+                };
+
+                context.Category.Add(newFoodGroup);
+                FoodGroupCodeToCategoryId.TryAdd(code, newFoodGroup.CategoryId);
+            }
+
+            context.SaveChanges();
+        }
+
+    }
+}
---
+++ PantryPlannerApiUnitTests/FoodCompositionETLUnitTest.cs
@@ -0,0 +1,59 @@
+﻿using PantryPlanner.Migrations;
+using PantryPlanner.Services;
+using PantryPlannerApiUnitTests.Helpers;
+using System.Linq;
+using Xunit;
+
+namespace PantryPlannerApiUnitTests
+{
+    public class FoodCompositionETLUnitTest
+    {
+        PantryPlannerContext _context;
+        USDAFoodCompositionDbETL _etl;
+
+        /// <summary>
+        /// Folder location of the .txt files with test data
+        /// </summary>
+        public static string FoodCompositionFolderLocation = "C:\\Users\\Adam\\Downloads\\SR-Leg_ASC";
+
+        public FoodCompositionETLUnitTest()
+        {
+            _context = InMemoryDataGenerator.CreateInMemoryDatabaseContext("FoodCompositionETLUnitTestDB");
+            _etl = new USDAFoodCompositionDbETL(FoodCompositionFolderLocation);
+        }
+
+        [Fact]
+        public void StartEtlProcess_InsertsCorrectResults()
+        {
+            // arrange: run the ETL process once
+            _etl.StartEtlProcess(_context);
+
+            // assert: correct amount of records inserted based on what it is in test files
+            int expectedCount = 7793;
+            Assert.Equal(expectedCount, _context.Ingredient.Count());
+
+            int expectedFoodGroupCount = 25;
+            Assert.Equal(expectedFoodGroupCount, _context.Category.Count());
+
+        }
+
+        [Fact]
+        public void StartEtlProcess_SkipsDuplicates()
+        {
+            // arrange: run the ETL process once
+            _etl.StartEtlProcess(_context);
+
+            int expectedCount = _context.Ingredient.Count();
+            int expectedFoodGroupCount = _context.Category.Count();
+
+            // act: running the ETL process again should run successfully without inserting duplicates
+            _etl.StartEtlProcess(_context);
+
+
+            // assert: nothing new was inserted
+            Assert.Equal(expectedCount, _context.Ingredient.Count());
+            Assert.Equal(expectedFoodGroupCount, _context.Category.Count());
+        }
+
+    }
+}
